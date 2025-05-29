76b8a31e339b4fd6c151a54948948b87f532409e
stormozov <web@stormozov.ru>
2025-05-29 17:19:29 +0300

    Настроил отображение фото в prod и dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,86 @@
-const path = require('node:path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+import { relative, resolve, dirname, posix } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
 
-module.exports = {
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: (pathData) => {
-            const fileName = path.basename(pathData.filename);
-            return `assets/img/${fileName}`;
+export default (env, argv) => {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const publicPath = argv.mode === 'production' ? '' : '/';
+
+  return {
+    output: {
+      path: resolve(__dirname, 'dist'),
+      publicPath: publicPath,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
           },
         },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: (pathData) => {
-            const fileName = path.basename(pathData.filename);
-            return `assets/fonts/${fileName}`;
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: { publicPath: publicPath },
+            },
+            'css-loader'
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: (pathData) => {
+              const relativePath = relative(resolve(__dirname, 'src'), pathData.filename)
+                .replace(/\\/g, '/');
+              return `assets/${relativePath}`;
+            },
           },
         },
-      }
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: (pathData) => {
+              const relativePath = relative(resolve(__dirname, 'src'), pathData.filename)
+                .replace(/\\/g, '/');
+              return `assets/${relativePath}`;
+            },
+          },
+        }
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'styles.css',
+      }),
+      new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.css',
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  optimization: {
-    minimizer: [
-      new TerserPlugin(),
-      new CssMinimizerPlugin(),
-    ],
-  },
+    optimization: {
+      minimizer: [
+        new TerserPlugin(),
+        new CssMinimizerPlugin(),
+      ],
+    },
+  };
 };
